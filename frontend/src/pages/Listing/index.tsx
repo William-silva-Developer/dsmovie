70cce39e2512c5662baf938709b0266d0a63534d
WilliamSilva-spring <william859@gmail.com>
2022-01-14 00:06:12 -0300

    Movie Card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,37 @@
+
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
-function Listing(){
-    return(
-        <Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 mg-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+
+            </div>
+        </>
     );
 }
 export default Listing;
